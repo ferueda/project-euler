899f6a70f8719f39b8aeee5e518be326a40d601b
Felipe Rueda <fe.rueda@hotmail.com>
2020-01-06 14:00:19 -0300

    Create 5.-smallest-multiple.js

---
+++ 5.-smallest-multiple.js
@@ -0,0 +1,13 @@
+/*
+2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+*/
+  
+function smallestMultiple() {
+  for (i = 20;i ; i++) {
+    for (j = 20; j >= 2; j--) {
+      if (i % j) break;
+      if (j === 2) return i;
+    }
+  }
+}
