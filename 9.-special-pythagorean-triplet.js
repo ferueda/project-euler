4d4aa0fc2b82d3b2db3a2a8cd5ffc5cc7d8967ee
Felipe Rueda <fe.rueda@hotmail.com>
2020-01-08 14:59:32 -0300

    Create 9.-special-pythagorean-triplet.js

---
+++ 9.-special-pythagorean-triplet.js
@@ -0,0 +1,40 @@
+/*
+A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+
+a2 + b2 = c2
+For example, 32 + 42 = 9 + 16 = 25 = 52.
+
+There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+Find the product abc.
+*/
+
+function specialPythagoreanTriplet() {
+  let a, b;
+  for (let b = 1; b < 1000; b++) {
+    a = (500000 - 1000 * b) / (1000 - b); //equation rearranged using algebra
+    
+    if (a % 1 === 0) return a * b * (1000 - (b + a));
+  }
+}
+
+
+/*
+We initially have
+a^2 + b^2 = c^2
+a + b + c = 1000
+
+First, rearrange c to left
+c = 1000 - (a + b)
+
+Insert c in first formula
+a^2 + b^2 = (1000 - (a + b))^2
+
+Multiplying we get
+a^2 + b^2 = 1000000 - 2000 * (a + b) + a^2 + 2 * a * b + b^2
+
+Rearrange formula to simplify and divide by 2
+1000 * (a + b) - a * b = 500000
+
+Leave a left
+a = (500000 - 1000 * b) / (1000 - b)
+*/
