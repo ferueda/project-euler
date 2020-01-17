6103838d828264b1af84de20499373a31c377e2e
Felipe Rueda <fe.rueda@hotmail.com>
2020-01-17 12:41:01 -0300

    Update 16.-power-digit-sum.js

--- 16.-power-digit-sum.js
+++ 16.-power-digit-sum.js
@@ -19,22 +19,16 @@ function numToPower(base, power) {
 }
 
 function largeSum(str1, str2) {
-  let sum = ""; // our result will be stored in a string.
-
-  // we'll need these in the program many times.
+  let sum = "";
   let str1Length = str1.length;
   let str2Length = str2.length;
 
   // if s2 is longer than s1, swap them.
   if (str2Length > str1Length) {
-    [str, str1] = [str1, str2]
+    [str2, str1] = [str1, str2]
   }
 
-  let carry = 0; // number that is carried to next decimal place, initially zero.
-  let a;
-  let b;
-  let temp;
-  let digitSum;
+  let a, b, temp, digitSum, carry = 0; 
   for (let i = 0; i < str1.length; i++) {
     a = parseInt(str1.charAt(str1.length - 1 - i)); // get ith digit of str1 from right, we store it in a
     b = parseInt(str2.charAt(str2.length - 1 - i)); // get ith digit of str2 from right, we store it in b
