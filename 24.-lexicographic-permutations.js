18bb71f8e20a1da52f67aa9f9f078fbd8d8d717e
Felipe Rueda <fe.rueda@hotmail.com>
2020-02-26 11:44:07 -0300

    Update 24.-lexicographic-permutations.js

--- 24.-lexicographic-permutations.js
+++ 24.-lexicographic-permutations.js
@@ -5,3 +5,25 @@ A permutation is an ordered arrangement of objects. For example, 3124 is one pos
 
 What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
 */
+
+function lexicographicPermutations(pos) {
+  const digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  let position = pos - 1;
+  let solution = '';
+
+  for (let i = digits.length - 1; i >= 0; i--) {
+    let temp = position / getFactorial(i) | 0;
+    position %= getFactorial(i);
+    solution += digits[temp];
+    digits.splice(temp, 1);
+  }
+
+  return solution;
+}
+
+function getFactorial(num) {
+  if (num <= 1) return 1;
+  else return num * getFactorial(num - 1);
+}
+
+console.log(lexicographicPermutations(1000000))
